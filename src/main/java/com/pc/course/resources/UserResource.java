396069333b81e02620d9408a287a9278fe14c844
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2020-07-16 20:43:00 -0300

    Implementação da entidade User

---
+++ src/main/java/com/pc/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.pc.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pc.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Paulo", "pc@pcsoft.com","55554444", "12345");
+		return ResponseEntity.ok(u);
+	}
+}
